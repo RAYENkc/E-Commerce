8c35292d7c1ee2f5439384e96bd8c5c253bef804
kacemrayen13@gmail.com <kacemrayen13@gmail.com>
2022-11-17 18:44:31 +0000

    first commit

---
+++ Controllers/ProduitController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TP_Ecommerce.DAL;
+using TP_Ecommerce.Models;
+namespace TP_Ecommerce.Controllers
+{
+    public class ProduitController : Controller
+    {
+        private EcommerceContext db = new EcommerceContext();
+        public ActionResult Index()
+        {
+            var produits = db.Produits.Include(p =>
+            p.Categorie).Include(p => p.Marque);
+            return View(produits.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.CategorieID = new SelectList(db.categories, "IDCat",
+            "NameCat");
+            ViewBag.MarqueID = new SelectList(db.Marques, "IDMrq",
+            "NameMrq");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="ID,CategorieID,MarqueID,ProductName,Price,qte")] produit produit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produits.Add(produit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategorieID = new SelectList(db.categories, "IDCat",
+            "NameCat", produit.CategorieID);
+            ViewBag.MarqueID = new SelectList(db.Marques, "IDMrq",
+            "NameMrq", produit.MarqueID);
+            return View(produit);
+        }
+    }
+}
---
+++ DAL/EcommerceContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TP_Ecommerce.Models;
+
+
+
+namespace TP_Ecommerce.DAL
+{
+    public class EcommerceContext :  DbContext 
+        
+       
+    {
+
+        public EcommerceContext() : base("EcommerceContext")
+        { }
+        public DbSet<Categorie> categories { get; set; }
+        public DbSet<Marque> Marques { get; set; }
+        public DbSet<produit> Produits { get; set; }
+
+    }
+
+  
+
+}
